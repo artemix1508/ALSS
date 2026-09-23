e4f10891e35511b4cd1a61cda1f459a8f024188f
Artem Ivashyn <artivas49@edu.gdansk.pl>
2026-09-23 10:59:11 +0200

    Add GPU detection

--- alss.py
+++ alss.py
@@ -1,4 +1,22 @@
 import os
+import subprocess
+
+def get_gpu_info():
+    result = subprocess.run(["lspci"], capture_output=True, text=True)
+    output = result.stdout
+
+    gpus= []
+
+    for line in output.splitlines():
+        if "VGA" in line:
+            if "NVIDIA" in line:
+                gpus.append("NVIDIA")
+            elif "AMD" in line:
+                gpus.append("AMD")
+            elif "Intel" in line:
+                gpus.append("Intel")
+            pass
+    return gpus
 
 def get_distro_info():
     if not os.path.exists('/etc/os-release'):
@@ -30,10 +48,10 @@ def get_package_manager(distro_info):
         "Omarchy": "pacman",
         "CachyOS": "pacman"
     }
-
     return package_managers.get(distro_name, "Unknown package manager")
 
 distro_info = get_distro_info()
 package_manager = get_package_manager(distro_info)
+gpu_info = get_gpu_info()
 print(f"Detected package manager: {package_manager}")
-
+print(f"Detected GPU's: {gpu_info}")
